94adf9645f20fd2155d7d33b0f10dadc6cd8c8ea
HatmanSTack <christopher.galliart@gmail.com>
2023-05-26 23:55:59 -0500

    Added Comments

--- src/main/java/gemenielabs/movies/SearchFragment.java
+++ src/main/java/gemenielabs/movies/SearchFragment.java
@@ -11,32 +11,38 @@ public class SearchFragment extends PreferenceFragmentCompat
         implements SharedPreferences.OnSharedPreferenceChangeListener,
         Preference.OnPreferenceChangeListener {
 
-
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Inflate preferences from XML resource
         addPreferencesFromResource(R.xml.preference_settings);
 
-        Preference popular = findPreference(getString(R.string.popular_key));
-        Preference top_rated = findPreference(getString(R.string.top_rated_key));
-        Preference favorites = findPreference(getString(R.string.favorites_key));
-        popular.setOnPreferenceChangeListener(this);
-        top_rated.setOnPreferenceChangeListener(this);
-        favorites.setOnPreferenceChangeListener(this);
+        // Set preference change listener for popular, top rated, and favorites preferences
+        setPreferenceChangeListener(getString(R.string.popular_key));
+        setPreferenceChangeListener(getString(R.string.top_rated_key));
+        setPreferenceChangeListener(getString(R.string.favorites_key));
+    }
+
+    // Set preference change listener for the given preference key
+    private void setPreferenceChangeListener(String preferenceKey) {
+        Preference preference = findPreference(preferenceKey);
+        preference.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-
+        // TODO: Handle shared preference changes if needed
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        // TODO: Handle preference changes if needed
         return true;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Register shared preference change listener
         getPreferenceScreen().getSharedPreferences()
                 .registerOnSharedPreferenceChangeListener(this);
     }
@@ -44,8 +50,8 @@ public class SearchFragment extends PreferenceFragmentCompat
     @Override
     public void onDestroy() {
         super.onDestroy();
+        // Unregister shared preference change listener
         getPreferenceScreen().getSharedPreferences()
                 .unregisterOnSharedPreferenceChangeListener(this);
     }
-
 }
